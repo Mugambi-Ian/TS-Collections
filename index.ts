72c6275797f2524f20eeb205c5facbbb53fb95be
Ian Mugambi <linksian63@gmail.com>
2020-03-31 11:41:52 +0300

    Collections

---
+++ index.ts
@@ -0,0 +1,6 @@
+import * as _arrays from './ts-collections/arrays';
+export const arrays = _arrays;
+export {default as Dictionary} from './ts-collections/dictionary';
+export {default as Set} from './ts-collections/set';
+import * as _util from './ts-collections/util';
+export const util = _util;
